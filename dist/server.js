7e36d34c96c112fd157bc14b3b9867bb142d859f
Felipe-Mercurio <95919509+Felipe-Mercurio@users.noreply.github.com>
2025-03-23 19:15:49 -0300

    fix Vercel

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,5 @@
 "use strict";
+console.log("✅ Servidor iniciando...");
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
